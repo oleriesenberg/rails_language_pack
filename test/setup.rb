ab4e5427a02eb2a3a48a955ad03bf3c56378ae9b
Kai König <kai@freundenews.de>
2009-01-24 22:44:38 +0100

    patched tests ... now working inside a rails(2.2.2) app ... cheers

--- test/setup.rb
+++ test/setup.rb
@@ -3,6 +3,19 @@ require 'test/unit'
 require 'shoulda'
 require 'mocha' 
 require 'I18n'
+
+# RAILS doesnt know I18n.load_translations so we so we patch it
+# outside of rails this is not neccesary
+require 'I18n'
+module I18n  
+  class << self
+    def load_translations(*args)
+      backend.load_translations(*args)
+    end
+  end
+end
+  
+
 begin
   require 'ruby-debug'
 rescue LoadError
